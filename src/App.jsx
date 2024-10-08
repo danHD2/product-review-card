767c0af0979af7d0a5cb0bbbe8b76c1f4df6ae56
unknown <dan.harabagiu.dimitrescu@gmail.com>
2024-10-08 18:11:50 +0300

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState, useEffect } from 'react';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function Card() {
+  
+  const [imageSrc, setImageSrc] = useState('../public/image-product-desktop.jpg');
+
+  const changeImageForMobile = () => {
+    const mobileImageSrc = '../public/image-product-mobile.jpg';
+    const desktopImageSrc = '../public/image-product-desktop.jpg';
+
+    if (window.innerWidth <= 800) {
+      setImageSrc(mobileImageSrc); 
+    } else {
+      setImageSrc(desktopImageSrc); 
+    }
+  };
+
+  useEffect(() => {
+    changeImageForMobile();
+
+    window.addEventListener('resize', changeImageForMobile);
+
+    return () => {
+      window.removeEventListener('resize', changeImageForMobile);
+    };
+  }, []); 
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='card'>
+          <div className='left-side'>
+              <img id='img' src={imageSrc} alt="Product"></img>
+          </div>
+          <div className='right-side'>
+              <h4>P E R F U M E</h4>
+              <h1>Gabrielle Essence Eau De Parfum</h1>
+              <p className='para'>A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.</p>
+              <h2>$149.99</h2>
+              <p className='oldprice'>$169.99</p>
+              <button>
+                  <img src='../../public/icon-cart.svg'></img>
+                  Add to Cart</button>
+          </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
-export default App
+export default Card;
